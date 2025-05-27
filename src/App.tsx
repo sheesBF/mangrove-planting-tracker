8033ed837acf618551b7bc0663ed6ba9affaa56e
sheesBF <shees@blueforest.co>
2025-05-27 17:00:18 +0400

    Update Landing Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Hero from './components/Hero';
-import Content from './components/Content';
-import Footer from './components/Footer';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <Hero />
-      <Content />
-      <Footer />
-    </>
+    </Router>
   );
 }
 
