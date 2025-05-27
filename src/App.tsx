a09e63fe703f763a578a704c321cac04051a8ef6
sheesBF <shees@blueforest.co>
2025-05-27 16:58:50 +0400

    Simplify to Landing Page Only

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Tracking from './pages/Tracking';
-import ProjectDetails from './pages/ProjectDetails';
-import PhaseDetails from './pages/PhaseDetails';
-import MonthlyStats from './pages/MonthlyStats';
-import { DataProvider } from './context/DataContext';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Content from './components/Content';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <DataProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/tracking" element={<Tracking />} />
-          <Route path="/project/1" element={<ProjectDetails />} />
-          <Route path="/phase/:id" element={<PhaseDetails />} />
-          <Route path="/monthly/:id" element={<MonthlyStats />} />
-        </Routes>
-      </Router>
-    </DataProvider>
+    <>
+      <Header />
+      <Hero />
+      <Content />
+      <Footer />
+    </>
   );
 }
 
