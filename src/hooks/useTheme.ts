cf3f6136476e0a8250f9deb58417e7897eb90346
sheesBF <shees@blueforest.co>
2025-05-29 00:49:51 +0400

    Create useTheme.ts

---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+
+export function useTheme() {
+  const [theme, setTheme] = useState<"light" | "dark">("dark");
+
+  useEffect(() => {
+    const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    setTheme(isDark ? "dark" : "light");
+  }, []);
+
+  return theme;
+}
