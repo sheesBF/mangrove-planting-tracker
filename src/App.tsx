65c1961c8bdb2b580bc16c6212e5d1d706b5b3f4
sheesBF <shees@blueforest.co>
2025-05-28 21:48:31 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Tracking from './pages/Tracking';
-import MonthlyData from './pages/MonthlyData';
+import Phase1MonthlyData from './pages/Phase1MonthlyData';
+import Phase2MonthlyData from './pages/Phase2MonthlyData';
+import Phase3MonthlyData from './pages/Phase3MonthlyData';
 
 function App() {
   return (
@@ -16,7 +18,9 @@ function App() {
           </div>
         } />
         <Route path="/tracking" element={<Tracking />} />
-         <Route path="/phase/:id" element={<MonthlyData />} />
+        <Route path="/phase/1" element={<Phase1MonthlyData />} />
+        <Route path="/phase/2" element={<Phase2MonthlyData />} />
+        <Route path="/phase/3" element={<Phase3MonthlyData />} />
       </Routes>
     </Router>
   );
